b1ae7e0af24e3252eb66f21899724268b2fafa47
iliangogov <iliangogov@gmail.com>
2016-10-19 16:51:09 +0300

    ADOdotNetHW/ADOdotNetHW.sln lection demo

---
+++ 10. ADO.NET/Homework/ADOdotNetHW/Homework/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Homework
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(
+                "Server=.;Database=TelerikAcademy;Integrated Security=true");
+            connection.Open();
+            Console.WriteLine("Connection Open");
+
+            using (connection)
+            {
+                SqlCommand comm = new SqlCommand("select top 10 * from Employees", connection);
+                SqlDataReader reader = comm.ExecuteReader();
+
+                int count = 0;
+                while (reader.Read())
+                {
+                    var firstName = reader["FirstName"];
+                    count++;
+                    for(int i = 0; i<reader.VisibleFieldCount;i++)
+                    {
+                        Console.WriteLine(reader[i]);
+                    }
+
+                    Console.WriteLine("Name of employee {0}", firstName);
+                }
+
+                Console.WriteLine("Number of employees {0}", count);
+            }
+        }
+    }
+}
