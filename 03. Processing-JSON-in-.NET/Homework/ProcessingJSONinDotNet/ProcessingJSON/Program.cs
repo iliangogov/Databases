578d39a43c977e1ae37887532e01cae6a4dc95f1
iliangogov <iliangogov@gmail.com>
2016-10-20 13:32:06 +0300

    part of hw

---
+++ 03. Processing-JSON-in-.NET/Homework/ProcessingJSONinDotNet/ProcessingJSON/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ProcessingJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rssLocalFile = "../../RssFeed.xml";
+            var url = "https://www.youtube.com/feeds/videos.xml?channel_id=UCLC-vbm7OWvpbqzXaoAMGGw";
+            var WebClient = new WebClient();
+            WebClient.DownloadFile(url,rssLocalFile);
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(rssLocalFile);
+            string jsonText = JsonConvert.SerializeXmlNode(doc);
+            var videoTitles = jsonText;//TODO
+            Console.WriteLine(jsonText);
+        }
+    }
+}
