056e4f25a8d28acba73d456792506c66262187dc
iliangogov <iliangogov@gmail.com>
2016-10-23 22:30:58 +0300

    Task 4,5 added

---
+++ 10. ADO.NET/Homework/ADOdotNetHW/HomeworkTask4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace HomeworkTask4
+{
+    public class Program
+    {
+        //Write a method that adds a new product in the products table in the Northwind database.
+        //Use a parameterized SQL command.
+
+        static void Main()
+        {
+            const string connectionString = "Server=.;Database=Northwind;Integrated Security=true";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            Console.WriteLine("Connection Open");
+           
+            using (connection)
+            {
+                const string commandStringInsert =
+                    @"INSERT INTO Products(ProductName, SupplierId, CategoryId, QuantityPerUnit, UnitPrice,
+                                           UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued)
+                             VALUES (@productName, @supplierId, @categoryId, @quantityPerUnit, @unitPrice, 
+                                     @unitsInStock, @unitsInOrder, @reorderLevel, @discontinued)";
+                SqlCommand command = new SqlCommand(commandStringInsert, connection);
+               
+                var productName = "Johny Walker - Blue Label ";
+                var supplierId = 22;
+                var categoryId = 1;
+                var quantityPerUnit = "700ml";
+                var unitPrice = 300d;
+                var unitsInStock = 100;
+                var unitsInOrder = 20;
+                var reorderLevel = 10;
+                var discontinued = 0;
+
+                
+                command.Parameters.AddWithValue("@productName", productName);
+                command.Parameters.AddWithValue("@supplierId", supplierId);
+                command.Parameters.AddWithValue("@categoryId", categoryId);
+                command.Parameters.AddWithValue("@quantityPerUnit", quantityPerUnit);
+                command.Parameters.AddWithValue("@unitPrice", unitPrice);
+                command.Parameters.AddWithValue("@unitsInStock", unitsInStock);
+                command.Parameters.AddWithValue("@unitsInOrder", unitsInOrder);
+                command.Parameters.AddWithValue("@reorderLevel", reorderLevel);
+                command.Parameters.AddWithValue("@discontinued", discontinued);
+
+                var queryResult = command.ExecuteNonQuery();
+                Console.WriteLine("({0} row(s) affected)", queryResult);
+
+                connection.Close();
+                Console.WriteLine("Connection closed");
+            }
+        }
+    }
+}
---
+++ 10. ADO.NET/Homework/ADOdotNetHW/HomeworkTask5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Drawing;
+
+namespace HomeworkTask5
+{
+    //Write a program that retrieves the images for all categories in the Northwind database 
+    //and stores them as JPG files in the file system.
+
+    public class Program
+    {
+        private const int OleMetaFilePictStartPosition = 78;
+
+        static void Main()
+        {
+            const string connectionString = "Server=.;Database=Northwind;Integrated Security=true";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            Console.WriteLine("Connection Open");
+
+            using (connection)
+            {
+                const string commandString = @"SELECT Picture FROM Categories";
+                SqlCommand command = new SqlCommand(commandString, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                int imageId = 1;
+
+                while (reader.Read())
+                {
+                    var fileBinaryData = (byte[])reader["Picture"];
+                    SaveImageWithOleMetaFilePict(imageId.ToString(), fileBinaryData, ".jpg");
+                    imageId++;
+                }
+            }
+
+            connection.Close();
+            Console.WriteLine(@"Pictures copyed to HomeworkTask5\bin\Debug");
+            Console.WriteLine("Connection Closed");
+        }
+
+        private static void SaveImageWithOleMetaFilePict(string fileName, byte[] imageBinaryData, string extension)
+        {
+            MemoryStream memoryStream =
+                new MemoryStream(imageBinaryData, OleMetaFilePictStartPosition, imageBinaryData.Length - OleMetaFilePictStartPosition);
+
+            using (memoryStream)
+            {
+                //you need to add reference System.Drawing
+                using (var image = Image.FromStream(memoryStream))
+                {
+                    image.Save(fileName + extension);
+                }
+            }
+        }
+    }
+}
