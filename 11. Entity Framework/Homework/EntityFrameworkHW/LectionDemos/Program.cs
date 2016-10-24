b65ff77a0347efaaf47e977118b2e625d9b2d189
iliangogov <iliangogov@gmail.com>
2016-10-25 00:46:21 +0300

    Lecture Drafts added

---
+++ 11. Entity Framework/Homework/EntityFrameworkHW/LectionDemos/Program.cs
@@ -0,0 +1,24 @@
+﻿using LectionDemos.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LectionDemos
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var dbContext = new NORTHWINDEntities();
+            var categories = dbContext.Categories;
+            foreach (var c in categories)
+            {
+                Console.WriteLine(c.CategoryName);
+            }
+
+
+        }
+    }
+}
