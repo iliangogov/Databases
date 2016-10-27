f57e303844cce479393f830a39aa25ffa73275b3
iliangogov <iliangogov@gmail.com>
2016-10-27 22:52:42 +0300

    EntityFrameworkHW.sln added

---
+++ 11. Entity Framework/Homework/Homework/EntityFrameworkHW/NorthwindMSSQL/CustomersManipulations.cs
@@ -0,0 +1,106 @@
+﻿using NorthwindMSSQL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NorthwindMSSQL
+{
+    public static class CustomersManipulations
+    {
+        public static void InsertCustomer(Customer newCustomer, NorthwindEntities db)
+        {
+            var customers = db.Customers;
+
+            customers.Add(newCustomer);
+            db.SaveChanges();
+
+            Console.WriteLine($"Customer name: {newCustomer.ContactName}, company: {newCustomer.CompanyName} has been inserted to Db");
+        }
+
+        public static void UpdateCustomerCompany(string customerName, NorthwindEntities db, string newCompanyName)
+        {
+            var customers = db.Customers;
+            var customer = customers.FirstOrDefault(c => c.ContactName == customerName);
+
+            customers.Attach(customer);
+            customer.CompanyName = newCompanyName;
+            db.SaveChanges();
+
+            Console.WriteLine($"Customer name: {customer.ContactName}, has changed his company to: {customer.CompanyName}");
+        }
+
+        public static void DeleteCustomer(string customerName, NorthwindEntities db)
+        {
+            var customers = db.Customers;
+            var customer = customers.FirstOrDefault(c => c.ContactName == customerName);
+
+            customers.Remove(customer);
+            db.SaveChanges();
+
+            Console.WriteLine($"Customer name: {customer.ContactName}, company: {customer.CompanyName} has been removed from Db");
+        }
+
+        public static void NativeInsertCustomer(Customer newCustomer, NorthwindEntities db)
+        {
+            var customers = db.Customers;
+            var query = "INSERT INTO Customers(CustomerID,ContactName,CompanyName) Values({0},{1},{2})";
+
+            db.Database.ExecuteSqlCommand(
+                query,
+                newCustomer.CustomerID,
+                newCustomer.ContactName,
+                newCustomer.CompanyName
+                );
+
+            db.SaveChanges();
+            Console.WriteLine("Native insert:");
+            Console.WriteLine($"Customer name: {newCustomer.ContactName}, company: {newCustomer.CompanyName} has been inserted to Db Natively");
+        }
+
+        public static void ShowCustomersWithOrdersByYearAndShipCountry(int year, string country, NorthwindEntities db)
+        {
+            var matchingCustomers = db.Orders
+                .Where(o => o.OrderDate.Value.Year == year && o.ShipCountry == country)
+                .Select(o => o.Customer)
+                .Distinct()
+                .ToList();
+            foreach (var ctr in matchingCustomers)
+            {
+                Console.WriteLine($"Customer {ctr.ContactName} has ordered in {year} year to {country}");
+            }
+        }
+
+        public static void ShowCustomersWithOrdersByYearAndShipCountryNative(int year, string country, NorthwindEntities db)
+        {
+            string query = $@"SELECT DISTINCT c.ContactName 
+                              FROM Orders o, Customers c 
+                              WHERE o.ShipCountry = '{country}' AND YEAR(o.OrderDate) = {year} AND c.CustomerID = o.CustomerID";
+
+            IEnumerable<string> matchingCustomers = db.Database.SqlQuery<string>(query);
+
+            db.SaveChanges();
+
+            Console.WriteLine("Native query Searched:");
+            foreach (var name in matchingCustomers)
+            {
+                Console.WriteLine($"Customer {name} has ordered in {year} year to {country}");
+            }
+        }
+
+        public static void ShowOrdersByDatesPeriodAndRegion(DateTime startDate, DateTime endDate, string region, NorthwindEntities db)
+        {
+            var matchingOrders =
+                from o in db.Orders
+                where o.ShipRegion == region && o.OrderDate.Value >= startDate && o.OrderDate.Value <= endDate
+                select o;
+
+            db.SaveChanges();
+
+            Console.WriteLine("Orders in desired period:");
+            foreach (var order in matchingOrders)
+            {
+                Console.WriteLine($"Order with ID: {order.OrderID} has been shiped to region {order.ShipRegion} on {order.OrderDate}");
+            }
+        }
+    }
+}
---
+++ 11. Entity Framework/Homework/Homework/EntityFrameworkHW/NorthwindMSSQL/TestRunner.cs
@@ -0,0 +1,38 @@
+﻿using NorthwindMSSQL.Data;
+using System;
+
+namespace NorthwindMSSQL
+{
+    public class TestRunner
+    {
+        static void Main(string[] args)
+        {
+            var dbContext = new NorthwindEntities();
+
+            var customers = dbContext.Customers;
+            var newCustomer = new Customer()
+            {
+                CustomerID="`PRGS",
+                ContactName= "Iliyan",
+                CompanyName = "Progress"
+            };
+            //Console.WriteLine(dbContext);
+            // Task 2
+            CustomersManipulations.InsertCustomer(newCustomer, dbContext);
+
+            CustomersManipulations.UpdateCustomerCompany("Iliyan", dbContext, "Microsoft");
+
+            CustomersManipulations.DeleteCustomer("Iliyan", dbContext);
+
+            // Task 3
+            CustomersManipulations.ShowCustomersWithOrdersByYearAndShipCountry(1997, "Canada", dbContext);
+
+            //Task 4
+            CustomersManipulations.ShowCustomersWithOrdersByYearAndShipCountryNative(1997, "Canada", dbContext);
+            CustomersManipulations.NativeInsertCustomer(newCustomer, dbContext);
+
+            //Task 5       
+            CustomersManipulations.ShowOrdersByDatesPeriodAndRegion(new DateTime(1997,05,10), new DateTime(1998,02,10), "RJ", dbContext);
+        }
+    }
+}
